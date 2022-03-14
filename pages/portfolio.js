ebb6eead126c74cac943d28935cc1d6ab6930230
Emma Liefmann <emmaliefmann@gmail.com>
2022-03-14 16:32:32 +0100

    updated article list

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -45,9 +45,33 @@ const portfolioPage = () => {
           </button>
         </div> */}
 
-        <div
-          className={styles.cardCtn}
-        >
+        <div className={styles.cardCtn}>
+          <motion.div
+            className={styles.card}
+            variants={item}
+            initial="hidden"
+            animate="show"
+          >
+            <div className={styles.image}>
+              <a
+                href="https://www.eatmcr.co.uk/news/the-food-and-drink-businesses-thriving-at-pollard-yard/"
+                target="_blank"
+              >
+                <img
+                  src="/img/portfolio/pollardyard.jpeg"
+                  alt="A collection of brightly coloured shipping containers in the shadow of an old mill"
+                />
+              </a>
+            </div>
+            <div className={styles.description}>
+              <h4>The food and drink businesses thriving at Pollard Yard</h4>
+              <p>
+                Speaking to some of the businesses at small business hub Pollard
+                Yard - a place where creativity oozes out of every perfectly
+                painted shipping container
+              </p>
+            </div>
+          </motion.div>
           <motion.div
             className={styles.card}
             variants={item}
@@ -59,7 +83,10 @@ const portfolioPage = () => {
                 href="https://www.eatmcr.co.uk/culture/new-lockdown-baking-business-manchester/"
                 target="_blank"
               >
-                <img src="/img/portfolio/homebakers.jpeg" alt="A woman in a bakery" />
+                <img
+                  src="/img/portfolio/homebakers.jpeg"
+                  alt="A woman in a bakery"
+                />
               </a>
             </div>
             <div className={styles.description}>
@@ -98,26 +125,26 @@ const portfolioPage = () => {
           <motion.div
             className={styles.card}
             variants={item}
+            variants={item}
             initial="hidden"
             animate="show"
           >
             <div className={styles.image}>
               <a
-                href="https://www.eatmcr.co.uk/news/the-food-and-drink-businesses-thriving-at-pollard-yard/"
+                href="https://www.ladbible.com/community/sport-expert-shares-biggest-tips-to-keep-you-running-over-winter-20201204"
                 target="_blank"
               >
                 <img
-                  src="/img/portfolio/pollardyard.jpeg"
-                  alt="A collection of brightly coloured shipping containers in the shadow of an old mill"
+                  src="/img/portfolio/runnersport.jpg"
+                  alt="The legs of a runner running along a road"
                 />
               </a>
             </div>
             <div className={styles.description}>
-              <h4>The food and drink businesses thriving at Pollard Yard</h4>
+              <h4>Winter running tips</h4>
               <p>
-                Speaking to some of the businesses at small business hub Pollard
-                Yard - a place where creativity oozes out of every perfectly
-                painted shipping container
+                "Expert Shares Biggest Tips To Keep You Running Over Winter" - a
+                branded content piece on LADbible for Brooks
               </p>
             </div>
           </motion.div>
@@ -129,7 +156,62 @@ const portfolioPage = () => {
             animate="show"
           >
             <div className={styles.image}>
-              <a href="https://www.eatmcr.co.uk/new-openings/crown-kettle-pub-manchester-northern-quarter/" target="_blank">
+              <a
+                href="https://www.ladbible.com/entertainment/tv-and-film-prison-break-voted-greatest-classic-tv-series-finale-on-disney-20210318?c=1616164092039"
+                target="_blank"
+              >
+                <img
+                  src="/img/portfolio/disney.jpg"
+                  alt="A TV on a stand with Disney+ showing on screen"
+                />
+              </a>
+            </div>
+            <div className={styles.description}>
+              <h4>Greatest series finale of all time</h4>
+              <p>
+                "Prison Break Voted 'Greatest Classic TV Series Finale' On Star
+                On Disney+" - a branded content piece on LADbible for Star on
+                Disney+
+              </p>
+            </div>
+          </motion.div>
+          <motion.div
+            className={styles.card}
+            variants={item}
+            variants={item}
+            initial="hidden"
+            animate="show"
+          >
+            <div className={styles.image}>
+              <a
+                href="https://peopleactivation.com/resources/no-points-for-busy-how-to-manage-energy-not-time"
+                target="_blank"
+              >
+                <img src="/img/portfolio/underground.jpg" />
+              </a>
+            </div>
+            <div className={styles.description}>
+              <h4>No points for busy: how to manage energy not time</h4>
+              <p>
+                Being busy is something that we often wear as a badge of pride.
+                Endless to-do lists, frantic work days and feeling like there
+                are never enough hours in the day - are we simply being busy
+                fools?
+              </p>
+            </div>
+          </motion.div>
+          <motion.div
+            className={styles.card}
+            variants={item}
+            variants={item}
+            initial="hidden"
+            animate="show"
+          >
+            <div className={styles.image}>
+              <a
+                href="https://www.eatmcr.co.uk/new-openings/crown-kettle-pub-manchester-northern-quarter/"
+                target="_blank"
+              >
                 <img src="/img/portfolio/crownandkettle.jpg" />
               </a>
             </div>
@@ -143,7 +225,10 @@ const portfolioPage = () => {
           </motion.div>
           <motion.div className={styles.card} variants={item}>
             <div className={styles.image}>
-              <a href="https://www.eatmcr.co.uk/new-openings/osma-prestwich-manchester-michelin-chef/" target="_blank">
+              <a
+                href="https://www.eatmcr.co.uk/new-openings/osma-prestwich-manchester-michelin-chef/"
+                target="_blank"
+              >
                 <img src="/img/portfolio/osma.jpg" />
               </a>
             </div>
@@ -157,88 +242,142 @@ const portfolioPage = () => {
           </motion.div>
           <motion.div className={styles.card} variants={item}>
             <div className={styles.image}>
-              <a href="https://www.eatmcr.co.uk/news/manchester-foodbanks-charities-homeless-winter/" target="_blank">
+              <a
+                href="https://www.eatmcr.co.uk/news/manchester-foodbanks-charities-homeless-winter/"
+                target="_blank"
+              >
                 <img src="/img/portfolio/foodbanks.jpg" />
               </a>
             </div>
             <div className={styles.description}>
               <h4>Food banks prepare for toughest winter yet</h4>
               <p>
-              The pandemic has had a huge impact on the organisations caring for the most vulnerable members of our community.
+                The pandemic has had a huge impact on the organisations caring
+                for the most vulnerable members of our community.
               </p>
             </div>
           </motion.div>
           <motion.div className={styles.card} variants={item}>
             <div className={styles.image}>
-              <a href="https://www.eatmcr.co.uk/news/meatco-manchester-meat-delivery-service/" target="_blank">
-                <img src="/img/portfolio/meatco.jpg" alt="Two women - the owners of MeatCo Manchester" />
+              <a
+                href="https://www.eatmcr.co.uk/news/meatco-manchester-meat-delivery-service/"
+                target="_blank"
+              >
+                <img
+                  src="/img/portfolio/meatco.jpg"
+                  alt="Two women - the owners of MeatCo Manchester"
+                />
               </a>
             </div>
             <div className={styles.description}>
               <h4>MeatCo launches quality meat delivery service</h4>
               <p>
-              You don’t have to go vegan to eat more sustainably – the two Manchester women changing the way we think about meat</p>
+                You don’t have to go vegan to eat more sustainably – the two
+                Manchester women changing the way we think about meat
+              </p>
             </div>
           </motion.div>
           <motion.div className={styles.card} variants={item}>
             <div className={styles.image}>
-              <a href="https://www.eatmcr.co.uk/news/manchester-pubs-bars-tier-3/" target="_blank">
-                <img src="/img/portfolio/beertaps.jpg" alt="A row of beer taps" />
+              <a
+                href="https://www.eatmcr.co.uk/news/manchester-pubs-bars-tier-3/"
+                target="_blank"
+              >
+                <img
+                  src="/img/portfolio/beertaps.jpg"
+                  alt="A row of beer taps"
+                />
               </a>
             </div>
             <div className={styles.description}>
               <h4>Can pubs and bars survive Tier 3?</h4>
               <p>
-              Speaking to various pub landlords and business owners about the impact of Tier 3 restrictions on their businesses</p>
+                Speaking to various pub landlords and business owners about the
+                impact of Tier 3 restrictions on their businesses
+              </p>
             </div>
           </motion.div>
           <motion.div className={styles.card} variants={item}>
             <div className={styles.image}>
-              <a href="https://www.eatmcr.co.uk/new-openings/ramona-manchester-detroit-style-pizza/" target="_blank">
-                <img src="/img/portfolio/ramona.jpg" alt="The inside of an old MOT garage where which is being renovated to become a pizza restaurant" />
+              <a
+                href="https://www.eatmcr.co.uk/new-openings/ramona-manchester-detroit-style-pizza/"
+                target="_blank"
+              >
+                <img
+                  src="/img/portfolio/ramona.jpg"
+                  alt="The inside of an old MOT garage where which is being renovated to become a pizza restaurant"
+                />
               </a>
             </div>
             <div className={styles.description}>
               <h4>Detroit-style pizzeria to open in former garage</h4>
               <p>
-              Ramona to open in Manchester bringing Detroit-style pizza to the Northern Quarter</p>
+                Ramona to open in Manchester bringing Detroit-style pizza to the
+                Northern Quarter
+              </p>
             </div>
           </motion.div>
           <motion.div className={styles.card} variants={item}>
             <div className={styles.image}>
-              <a href="https://www.eatmcr.co.uk/new-openings/lilys-vegetarian-indian-chorlton-deli/" target="_blank">
-                <img src="/img/portfolio/lilys.jpeg" alt="A display counter with Indian snacks and sweets" />
+              <a
+                href="https://www.eatmcr.co.uk/new-openings/lilys-vegetarian-indian-chorlton-deli/"
+                target="_blank"
+              >
+                <img
+                  src="/img/portfolio/lilys.jpeg"
+                  alt="A display counter with Indian snacks and sweets"
+                />
               </a>
             </div>
             <div className={styles.description}>
               <h4>Lily's deli opens in Chorlton</h4>
               <p>
-              "We are just so blessed, aren't we? ... You know, we appreciate all of our customers and it's just fantastic that we've been able to do it in lockdown."</p>
+                "We are just so blessed, aren't we? ... You know, we appreciate
+                all of our customers and it's just fantastic that we've been
+                able to do it in lockdown."
+              </p>
             </div>
           </motion.div>
 
           <motion.div className={styles.card} variants={item}>
             <div className={styles.image}>
-              <a href="https://www.eatmcr.co.uk/new-openings/launched-in-lockdown/" target="_blank">
-                <img src="/img/portfolio/launchedlockdown.png" alt="A split image showing a pastrami sandwich on the left and a stack of ice cream sandwiches on the right" />
-              </a> 
+              <a
+                href="https://www.eatmcr.co.uk/new-openings/launched-in-lockdown/"
+                target="_blank"
+              >
+                <img
+                  src="/img/portfolio/launchedlockdown.png"
+                  alt="A split image showing a pastrami sandwich on the left and a stack of ice cream sandwiches on the right"
+                />
+              </a>
             </div>
             <div className={styles.description}>
               <h4>Launched in Lockdown</h4>
               <p>
-              A guide to just some of the many Manchester food and drink projects that launched through lockdowns and Covid uncertainty</p>
+                A guide to just some of the many Manchester food and drink
+                projects that launched through lockdowns and Covid uncertainty
+              </p>
             </div>
           </motion.div>
           <motion.div className={styles.card} variants={item}>
             <div className={styles.image}>
-              <a href="https://www.eatmcr.co.uk/culture/the-salford-burger-kitchen-that-can-get-more-than-1000-orders-a-day/" target="_blank">
-                <img src="/img/portfolio/burgerism.jpg" alt="A row of cheese burgers being put together" />
+              <a
+                href="https://www.eatmcr.co.uk/culture/the-salford-burger-kitchen-that-can-get-more-than-1000-orders-a-day/"
+                target="_blank"
+              >
+                <img
+                  src="/img/portfolio/burgerism.jpg"
+                  alt="A row of cheese burgers being put together"
+                />
               </a>
             </div>
             <div className={styles.description}>
               <h4>The burger kitchen doing 1000 orders a day</h4>
               <p>
-              Burgerism launched in April 2018, a delivery-only burger concept based in a warehouse in Salford, aiming to offer customers “the best off-premises burger in your neighbourhood”.</p>
+                Burgerism launched in April 2018, a delivery-only burger concept
+                based in a warehouse in Salford, aiming to offer customers “the
+                best off-premises burger in your neighbourhood”.
+              </p>
             </div>
           </motion.div>
         </div>
